8612feb3fbff50793d1201871a5a9c6476dc0ffa
tahelegonus <telegonuscpt866@gmail.com>
2026-07-30 13:12:11 -0400

    shade_matcherv2.py

---
+++ shade_matcherv2.py
@@ -0,0 +1,46 @@
+import pandas as pd 
+
+from sklearn.tree import DecisionTreeClassifier
+
+df = pd.read_csv("foundation.csv")
+
+
+
+print(df.head())
+
+df["BrandCode"] = df["Brand"].map({
+    "Huda Beauty": 0,
+    "Fenty Beauty": 1,
+    "MAC": 2
+})
+
+
+X = df[["BrandCode", "DepthScore", "UndertoneScore"]]
+
+y = df["Shade"]
+
+model = DecisionTreeClassifier()
+
+model.fit(X,y)
+
+prediction = model.predict(
+    pd.DataFrame(
+        [[1,4,2]],
+        columns=["BrandCode", "DepthScore", "UndertoneScore"]
+    )
+)
+
+print(prediction)
+
+brand = int (input("Brand: "))
+depth = int(input("Depth: "))
+undertone = int(input("Undertone: "))
+
+prediciton = model.predict([[brand, depth, undertone]])
+
+
+
+
+
+
+print("Predicted Shade: ", prediction[0])
