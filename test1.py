0cefc5fbb52829bd61c269e8defec405514dcf50
tahelegonus <telegonuscpt866@gmail.com>
2026-07-30 13:10:35 -0400

    test1.py

---
+++ test1.py
@@ -0,0 +1,16 @@
+      Brand          Shade  ... Undertone  UndertoneScore
+0  Huda Beauty      Milkshake  ...      Cool               0
+1  Huda Beauty   Yogurt Drops  ...   Neutral               1
+2  Huda Beauty  Vanilla Swirl  ...      Warm               2
+3  Huda Beauty     Shortbread  ...   Neutral               1
+4  Huda Beauty       Macaroon  ...      Cool               0
+
+[5 rows x 6 columns]
+C:\Users\tahelegonus\AppData\Roaming\Python\Python312\site-packages\sklearn\tree\_classes.py:289: UserWarning: The number of unique classes is greater than 50% of the number of samples. `y` could represent a regression problem, not a classification problem.
+  check_classification_targets(y)
+C:\Users\83 N-CPT Analyst\AppData\Roaming\Python\Python312\site-packages\sklearn\utils\validation.py:2827: UserWarning: X does not have valid feature names, but DecisionTreeClassifier was fitted with feature names
+  warnings.warn(
+['300']
+Brand: 
+
+
