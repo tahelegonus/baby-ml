91628db271c190a39eda847f8904f443be6ad889
tahelegonus <telegonuscpt866@gmail.com>
2026-07-30 13:13:11 -0400

    shade_matcherv2_output.py

---
+++ shade_matcherv2_output.py
@@ -0,0 +1,9 @@
+:289: UserWarning: The number of unique classes is greater than 50% of the number of samples. `y` could represent a regression problem, not a classification problem.
+  check_classification_targets(y)
+['300']
+Brand: 1
+Depth: 4
+Undertone: 2
+C:\Users\tahelegonus\AppData\Roaming\Python\Python312\site-packages\sklearn\utils\validation.py:2827: UserWarning: X does not have valid feature names, but DecisionTreeClassifier was fitted with feature names
+  warnings.warn(
+Predicted Shade:  300
