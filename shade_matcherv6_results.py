649df43789453aae0d59cd19e799834b9cc2ba04
tahelegonus <telegonuscpt866@gmail.com>
2026-07-31 09:36:18 -0400

    shade_matcherv6_results.py

---
+++ shade_matcherv6_results.py
@@ -0,0 +1,16 @@
+
+Available Brands
+- Huda Beauty
+- Fenty Beauty
+- MAC
+
+Brand: Huda Beauty
+Depth: Medium
+Undertone: Warm
+
+Recommended Shade: Tres Leches
+
+Was this recommendation correct? (y/n): y
+
+Confirmation Saved!
+PS C:\Users\83 N-CPT Analyst\Desktop\shadematcher> 
