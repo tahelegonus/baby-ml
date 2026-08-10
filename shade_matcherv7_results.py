b6b8356d4f6a5023db7df856fc37b6cf15710bd6
Treasure Harvey <telegonuscpt866@gmail.com>
2026-08-10 14:59:06 -0400

    Create shade_matcherv7_results.py

---
+++ shade_matcherv7_results.py
@@ -0,0 +1,38 @@
+##I added hev values to allow the program to find simialr shades, doing comparisions.
+## it needs more work, but it does give results for some shades, using a simalrtiy score. 
+
+
+Available Brands
+- Huda Beauty
+- Fenty Beauty
+- MAC
+
+Brand: huda beauty
+Depth: medium dark
+Undertone: warm
+
+Recommended Shade: Toffee
+Closest Shades:
+No similar shades found.
+
+Was this recommendation correct? (y/n): y
+
+Confirmation saved!
+PS C:\Users\83 N-CPT Analyst\Desktop\shadematcher> & "C:\Program Files\Python312\python.exe" "c:/Users/83 N-CPT Analyst/Desktop/shadematcher/shade_matcher.py"
+Example Prediction: 300
+
+Available Brands
+- Huda Beauty
+- Fenty Beauty
+- MAC
+
+Brand: huda beauty
+Depth: medium
+Undertone: warm
+
+Recommended Shade: Tres Leches
+
+Closest Shades:
+No similar shades found.
+
+Was this recommendation correct? (y/n): 
