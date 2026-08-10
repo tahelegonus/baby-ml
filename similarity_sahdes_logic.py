f6d5c9c30334c385f980ef344fdbb7717164c255
Treasure Harvey <telegonuscpt866@gmail.com>
2026-08-10 15:02:55 -0400

    similarity_sahdes_logic.py

---
+++ similarity_sahdes_logic.py
@@ -0,0 +1,269 @@
+PS C:\Users\tahelegonus\Desktop\shadematcher> & "C:\Program Files\Python312\python.exe" "c:/Users/tahelegonus/Desktop/shadematcher/shade__similarity.py"
+           Brand     Shade         Depth  ...  Undertone UndertoneScore  HexColor
+0    Huda Beauty  Cinnamon          Dark  ...       Warm              2   #8B563F
+1    Huda Beauty     Cocoa          Dark  ...    Neutral              1   #754638
+2   Fenty Beauty       100    Very Light  ...       Cool              0   #F1D6C8
+3   Fenty Beauty       120    Very Light  ...    Neutral              1   #EBC8B7
+4   Fenty Beauty       150         Light  ...       Warm              2   #D9A88F
+5   Fenty Beauty       190         Light  ...       Cool              0   #D5A08E
+6   Fenty Beauty       210  Medium Light  ...    Neutral              1   #C99178
+7   Fenty Beauty       240        Medium  ...    Neutral              1   #BD856C
+8   Fenty Beauty       300        Medium  ...       Warm              2   #A96F56
+9   Fenty Beauty       330   Medium Dark  ...       Warm              2   #965D46
+10  Fenty Beauty       420          Dark  ...    Neutral              1   #704334
+11  Fenty Beauty       450          Dark  ...       Warm              2   #61382D
+12           MAC      NC10    Very Light  ...       Warm              2   #F0D3BE
+13           MAC      NW10    Very Light  ...       Cool              0   #EACBC5
+14           MAC      NC20         Light  ...       Warm              2   #E2B99E
+15           MAC      NW20         Light  ...       Cool              0   #DDB5AA
+16           MAC      NC30  Medium Light  ...       Warm              2   #C99A7C
+17           MAC      NW25  Medium Light  ...       Cool              0   #C99B91
+19           MAC      NW35        Medium  ...       Cool              0   #B9867D
+20           MAC      NC45   Medium Dark  ...       Warm              2   #9C684F
+21           MAC      NW50          Dark  ...       Cool              0   #6F4038
+
+[22 rows x 7 columns]
+Color Distance: 39.496835316262995
+PS C:\Users\tahelegonus\Desktop\shadematcher> & "C:\Program Files\Python312\python.exe" "c:/Users/tahelegonus/Desktop/shadematcher/shade__similarity.py"
+           Brand     Shade         Depth  ...  Undertone UndertoneScore  HexColor
+0    Huda Beauty  Cinnamon          Dark  ...       Warm              2   #8B563F
+1    Huda Beauty     Cocoa          Dark  ...    Neutral              1   #754638
+2   Fenty Beauty       100    Very Light  ...       Cool              0   #F1D6C8
+3   Fenty Beauty       120    Very Light  ...    Neutral              1   #EBC8B7
+4   Fenty Beauty       150         Light  ...       Warm              2   #D9A88F
+5   Fenty Beauty       190         Light  ...       Cool              0   #D5A08E
+6   Fenty Beauty       210  Medium Light  ...    Neutral              1   #C99178
+7   Fenty Beauty       240        Medium  ...    Neutral              1   #BD856C
+8   Fenty Beauty       300        Medium  ...       Warm              2   #A96F56
+9   Fenty Beauty       330   Medium Dark  ...       Warm              2   #965D46
+10  Fenty Beauty       420          Dark  ...    Neutral              1   #704334
+11  Fenty Beauty       450          Dark  ...       Warm              2   #61382D
+12           MAC      NC10    Very Light  ...       Warm              2   #F0D3BE
+13           MAC      NW10    Very Light  ...       Cool              0   #EACBC5
+14           MAC      NC20         Light  ...       Warm              2   #E2B99E
+15           MAC      NW20         Light  ...       Cool              0   #DDB5AA
+16           MAC      NC30  Medium Light  ...       Warm              2   #C99A7C
+18           MAC      NC35        Medium  ...       Warm              2   #BD896C
+19           MAC      NW35        Medium  ...       Cool              0   #B9867D
+20           MAC      NC45   Medium Dark  ...       Warm              2   #9C684F
+21           MAC      NW50          Dark  ...       Cool              0   #6F4038
+
+[22 rows x 7 columns]
+PS C:\Users\tahelegonus\Desktop\shadematcher> & "C:\Program Files\Python312\python.exe" "c:/Users/tahelegonus/Desktop/shadematcher/shade__similarity.py"
+           Brand     Shade         Depth  ...  Undertone UndertoneScore  HexColor
+0    Huda Beauty  Cinnamon          Dark  ...       Warm              2   #8B563F
+1    Huda Beauty     Cocoa          Dark  ...    Neutral              1   #754638
+2   Fenty Beauty       100    Very Light  ...       Cool              0   #F1D6C8
+3   Fenty Beauty       120    Very Light  ...    Neutral              1   #EBC8B7
+4   Fenty Beauty       150         Light  ...       Warm              2   #D9A88F
+5   Fenty Beauty       190         Light  ...       Cool              0   #D5A08E
+6   Fenty Beauty       210  Medium Light  ...    Neutral              1   #C99178
+7   Fenty Beauty       240        Medium  ...    Neutral              1   #BD856C
+8   Fenty Beauty       300        Medium  ...       Warm              2   #A96F56
+9   Fenty Beauty       330   Medium Dark  ...       Warm              2   #965D46
+10  Fenty Beauty       420          Dark  ...    Neutral              1   #704334
+11  Fenty Beauty       450          Dark  ...       Warm              2   #61382D
+12           MAC      NC10    Very Light  ...       Warm              2   #F0D3BE
+13           MAC      NW10    Very Light  ...       Cool              0   #EACBC5
+14           MAC      NC20         Light  ...       Warm              2   #E2B99E
+15           MAC      NW20         Light  ...       Cool              0   #DDB5AA
+16           MAC      NC30  Medium Light  ...       Warm              2   #C99A7C
+17           MAC      NW25  Medium Light  ...       Cool              0   #C99B91
+18           MAC      NC35        Medium  ...       Warm              2   #BD896C
+19           MAC      NW35        Medium  ...       Cool              0   #B9867D
+20           MAC      NC45   Medium Dark  ...       Warm              2   #9C684F
+
+[22 rows x 7 columns]
+
+Target Shade:
+          Brand Shade   Depth  DepthScore Undertone  UndertoneScore HexColor
+8  Fenty Beauty   300  Medium           4      Warm               2  #A96F56
+PS C:\Users\tahelegonus\Desktop\shadematcher> & "C:\Program Files\Python312\python.exe" "c:/Users/tahelegonus/Desktop/shadematcher/shade__similarity.py"
+           Brand     Shade         Depth  ...  Undertone UndertoneScore  HexColor
+0    Huda Beauty  Cinnamon          Dark  ...       Warm              2   #8B563F
+1    Huda Beauty     Cocoa          Dark  ...    Neutral              1   #754638
+2   Fenty Beauty       100    Very Light  ...       Cool              0   #F1D6C8
+3   Fenty Beauty       120    Very Light  ...    Neutral              1   #EBC8B7
+4   Fenty Beauty       150         Light  ...       Warm              2   #D9A88F
+5   Fenty Beauty       190         Light  ...       Cool              0   #D5A08E
+6   Fenty Beauty       210  Medium Light  ...    Neutral              1   #C99178
+7   Fenty Beauty       240        Medium  ...    Neutral              1   #BD856C
+8   Fenty Beauty       300        Medium  ...       Warm              2   #A96F56
+9   Fenty Beauty       330   Medium Dark  ...       Warm              2   #965D46
+10  Fenty Beauty       420          Dark  ...    Neutral              1   #704334
+11  Fenty Beauty       450          Dark  ...       Warm              2   #61382D
+12           MAC      NC10    Very Light  ...       Warm              2   #F0D3BE
+13           MAC      NW10    Very Light  ...       Cool              0   #EACBC5
+14           MAC      NC20         Light  ...       Warm              2   #E2B99E
+15           MAC      NW20         Light  ...       Cool              0   #DDB5AA
+16           MAC      NC30  Medium Light  ...       Warm              2   #C99A7C
+17           MAC      NW25  Medium Light  ...       Cool              0   #C99B91
+18           MAC      NC35        Medium  ...       Warm              2   #BD896C
+19           MAC      NW35        Medium  ...       Cool              0   #B9867D
+20           MAC      NC45   Medium Dark  ...       Warm              2   #9C684F
+21           MAC      NW50          Dark  ...       Cool              0   #6F4038
+
+[22 rows x 7 columns]
+
+Target Shade:
+          Brand Shade   Depth  DepthScore Undertone  UndertoneScore HexColor
+8  Fenty Beauty   300  Medium           4      Warm               2  #A96F56
+
+Target HexColor: #A96F56
+
+           Brand     Shade        Depth Undertone  ColorDistance
+20           MAC      NC45  Medium Dark      Warm      16.340135
+9   Fenty Beauty       330  Medium Dark      Warm      30.675723
+7   Fenty Beauty       240       Medium   Neutral      36.986484
+18           MAC      NC35       Medium      Warm      39.496835
+0    Huda Beauty  Cinnamon         Dark      Warm      45.321077
+PS C:\Users\tahelegonus\Desktop\shadematcher> & "C:\Program Files\Python312\python.exe" "c:/Users/tahelegonus/Desktop/shadematcher/shade__similarity.py"
+
+Foundation Data Loaded!
+          Brand     Shade       Depth  ...  Undertone UndertoneScore  HexColor
+1   Huda Beauty     Cocoa        Dark  ...    Neutral              1   #754638
+2  Fenty Beauty       100  Very Light  ...       Cool              0   #F1D6C8
+3  Fenty Beauty       120  Very Light  ...    Neutral              1   #EBC8B7
+4  Fenty Beauty       150       Light  ...       Warm              2   #D9A88F
+
+[5 rows x 7 columns]
+PS C:\Users\tahelegonus\Desktop\shadematcher> & "C:\Program Files\Python312\python.exe" "c:/Users/tahelegonus/Desktop/shadematcher/shade__similarity.py"
+          Brand     Shade       Depth  ...  Undertone UndertoneScore  HexColor
+0   Huda Beauty  Cinnamon        Dark  ...       Warm              2   #8B563F
+1   Huda Beauty     Cocoa        Dark  ...    Neutral              1   #754638
+2  Fenty Beauty       100  Very Light  ...       Cool              0   #F1D6C8
+3  Fenty Beauty       120  Very Light  ...    Neutral              1   #EBC8B7
+
+[5 rows x 7 columns]
+
+Target Shade:
+          Brand Shade   Depth  DepthScore Undertone  UndertoneScore HexColor
+8  Fenty Beauty   300  Medium           4      Warm               2  #A96F56
+PS C:\Users\tahelegonus\Desktop\shadematcher> & "C:\Program Files\Python312\python.exe" "c:/Users/tahelegonus/Desktop/shadematcher/shade__similarity.py"
+
+Foundation Data Loaded!
+          Brand     Shade       Depth  ...  Undertone UndertoneScore  HexColor
+0   Huda Beauty  Cinnamon        Dark  ...       Warm              2   #8B563F
+1   Huda Beauty     Cocoa        Dark  ...    Neutral              1   #754638
+2  Fenty Beauty       100  Very Light  ...       Cool              0   #F1D6C8
+3  Fenty Beauty       120  Very Light  ...    Neutral              1   #EBC8B7
+
+[5 rows x 7 columns]
+
+Target Shade:
+          Brand Shade   Depth  DepthScore Undertone  UndertoneScore HexColor
+8  Fenty Beauty   300  Medium           4      Warm               2  #A96F56
+PS C:\Users\tahelegonus\Desktop\shadematcher> & "C:\Program Files\Python312\python.exe" "c:/Users/tahelegonus/Desktop/shadematcher/shade__similarity.py"
+          Brand     Shade       Depth  ...  Undertone UndertoneScore  HexColor
+0   Huda Beauty  Cinnamon        Dark  ...       Warm              2   #8B563F
+1   Huda Beauty     Cocoa        Dark  ...    Neutral              1   #754638
+2  Fenty Beauty       100  Very Light  ...       Cool              0   #F1D6C8
+3  Fenty Beauty       120  Very Light  ...    Neutral              1   #EBC8B7
+4  Fenty Beauty       150       Light  ...       Warm              2   #D9A88F
+
+
+Target Shade:
+          Brand Shade   Depth  DepthScore Undertone  UndertoneScore HexColor
+8  Fenty Beauty   300  Medium           4      Warm               2  #A96F56
+
+Target HexColor: #A96F56
+PS C:\Users\tahelegonus\Desktop\shadematcher> & "C:\Program Files\Python312\python.exe" "c:/Users/tahelegonus/Desktop/shadematcher/shade__similarity.py"
+0   Huda Beauty  Cinnamon        Dark  ...       Warm              2   #8B563F
+1   Huda Beauty     Cocoa        Dark  ...    Neutral              1   #754638
+2  Fenty Beauty       100  Very Light  ...       Cool              0   #F1D6C8
+3  Fenty Beauty       120  Very Light  ...    Neutral              1   #EBC8B7
+4  Fenty Beauty       150       Light  ...       Warm              2   #D9A88F
+
+[5 rows x 7 columns]
+
+          Brand Shade   Depth  DepthScore Undertone  UndertoneScore HexColor
+8  Fenty Beauty   300  Medium           4      Warm               2  #A96F56
+
+Target HexColor: #A96F56
+
+Color distances calculated!
+PS C:\Users\tahelegonus\Desktop\shadematcher> & "C:\Program Files\Python312\python.exe" "c:/Users/tahelegonus/Desktop/shadematcher/shade__similarity.py"
+Foundation Data Loaded!
+          Brand     Shade       Depth  ...  Undertone UndertoneScore  HexColor
+0   Huda Beauty  Cinnamon        Dark  ...       Warm              2   #8B563F
+1   Huda Beauty     Cocoa        Dark  ...    Neutral              1   #754638
+2  Fenty Beauty       100  Very Light  ...       Cool              0   #F1D6C8
+3  Fenty Beauty       120  Very Light  ...    Neutral              1   #EBC8B7
+4  Fenty Beauty       150       Light  ...       Warm              2   #D9A88F
+
+[5 rows x 7 columns]
+
+Target Shade:
+          Brand Shade   Depth  DepthScore Undertone  UndertoneScore HexColor
+8  Fenty Beauty   300  Medium           4      Warm               2  #A96F56
+
+Target HexColor: #A96F56
+
+Color distances calculated!
+
+Closest Shades:
+           Brand     Shade         Depth Undertone  ColorDistance
+20           MAC      NC45   Medium Dark      Warm      16.340135
+9   Fenty Beauty       330   Medium Dark      Warm      30.675723
+7   Fenty Beauty       240        Medium   Neutral      36.986484
+0    Huda Beauty  Cinnamon          Dark      Warm      45.321077
+19           MAC      NW35        Medium      Cool      48.020829
+6   Fenty Beauty       210  Medium Light   Neutral      57.758116
+16           MAC      NC30  Medium Light      Warm      65.703881
+1    Huda Beauty     Cocoa          Dark   Neutral      72.698005
+10  Fenty Beauty       420          Dark   Neutral      79.630396
+PS C:\Users\tahelegonus\Desktop\shadematcher> & "C:\Program Files\Python312\python.exe" "c:/Users/tahelegonus/Desktop/shadematcher/shade__similarity.py"
+          Brand     Shade       Depth  ...  Undertone UndertoneScore  HexColor
+0   Huda Beauty  Cinnamon        Dark  ...       Warm              2   #8B563F
+1   Huda Beauty     Cocoa        Dark  ...    Neutral              1   #754638
+2  Fenty Beauty       100  Very Light  ...       Cool              0   #F1D6C8
+3  Fenty Beauty       120  Very Light  ...    Neutral              1   #EBC8B7
+4  Fenty Beauty       150       Light  ...       Warm              2   #D9A88F
+
+[5 rows x 7 columns]
+
+Target Shade:
+          Brand Shade   Depth  DepthScore Undertone  UndertoneScore HexColor
+8  Fenty Beauty   300  Medium           4      Warm               2  #A96F56
+
+Target HexColor: #A96F56
+
+Color distances calculated!
+
+Closest Shades:
+           Brand     Shade         Depth Undertone  ColorDistance  SimilarityScore
+20           MAC      NC45   Medium Dark      Warm      16.340135        12.304081
+9   Fenty Beauty       330   Medium Dark      Warm      30.675723        20.905434
+7   Fenty Beauty       240        Medium   Neutral      36.986484        23.691890
+19           MAC      NW35        Medium      Cool      48.020829        31.812497
+0    Huda Beauty  Cinnamon          Dark      Warm      45.321077        32.192646
+6   Fenty Beauty       210  Medium Light   Neutral      57.758116        38.654870
+16           MAC      NC30  Medium Light      Warm      65.703881        41.922329
+1    Huda Beauty     Cocoa          Dark   Neutral      72.698005        50.118803
+17           MAC      NW25  Medium Light      Cool      80.255841        53.653505
+PS C:\Users\tahelegonus\Desktop\shadematcher> & "C:\Program Files\Python312\python.exe" "c:/Users/tahelegonus/Desktop/shadematcher/shade__similarity.py"
+0   Huda Beauty  Cinnamon        Dark  ...       Warm              2   #8B563F
+1   Huda Beauty     Cocoa        Dark  ...    Neutral              1   #754638
+2  Fenty Beauty       100  Very Light  ...       Cool              0   #F1D6C8
+3  Fenty Beauty       120  Very Light  ...    Neutral              1   #EBC8B7
+4  Fenty Beauty       150       Light  ...       Warm              2   #D9A88F
+
+[5 rows x 7 columns]
+
+Target Shade:
+          Brand Shade       Depth  DepthScore Undertone  UndertoneScore HexColor
+2  Fenty Beauty   100  Very Light           1      Cool               0  #F1D6C8
+
+Target HexColor: #F1D6C8
+
+Color distances calculated!
+
+Closest Shades:
+           Brand Shade         Depth Undertone  ColorDistance  SimilarityScore
+2   Fenty Beauty   100    Very Light      Cool       0.000000         0.000000
+13           MAC  NW10    Very Light      Cool      13.379088         8.027453
+14           MAC  NC20         Light      Warm      53.197744        37.418647
+4   Fenty Beauty   150         Light      Warm      77.077883        51.746730
+5   Fenty Beauty   190         Light      Cool      84.047606        52.928563
+17           MAC  NW25  Medium Light      Cool      90.033327        59.019996
+16           MAC  NC30  Medium Light      Warm     104.766407        70.859844
